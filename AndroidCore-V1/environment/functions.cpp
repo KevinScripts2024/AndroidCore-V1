79cac2cc71b9b2b2239b816b5d351cb93bb65f2c
wildhog <100944663+literallyawildhog@users.noreply.github.com>
2024-08-23 20:33:50 +0800

    Added environment and functions

---
+++ AndroidCore-V1/environment/functions.cpp
@@ -0,0 +1,44 @@
+/*
+#include <lua.h>
+#include <lualib.h>
+#include <lmem.h>
+#include <lgc.h>
+*/
+
+static int getgenv() // register the luau first to register the lua state here
+{
+	// lua_pushvalue(ls, LUA_GLOBALSINDEX);
+	return 1;
+}
+
+static int getreg() // register the luau first to register the lua state here
+{
+	// lua_pushvalue(ls, LUA_REGISTRYINDEX);
+	return 1;
+}
+
+static int gettenv() // register the luau first to register the lua state here
+{
+	/*
+	luaL_checktype(ls, 1, LUA_TTHREAD);
+
+    auto thread = lua_tothread(ls, 1);
+
+    lua_pushvalue(thread, LUA_GLOBALSINDEX);
+    lua_xmove(thread, ls, 1);
+	*/
+	return 1;
+}
+
+static int getthread() // register the luau first to register the lua state here
+{
+	// lua_pushthread(ls);
+	return 1;
+}
+
+static const luaL_Reg function[] = { // Should be getting an error because of unregistered luau.
+	{"getgenv", getgenv},
+	{"getreg", getreg},
+	{"gettenv", gettenv},
+	{"getthread", getthread},
+}
