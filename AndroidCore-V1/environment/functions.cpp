d295df7723e8251290dab264d39fc648cce1cf84
wildhog <100944663+literallyawildhog@users.noreply.github.com>
2024-08-24 10:50:29 +0800

    Added isLuau

--- AndroidCore-V1/environment/functions.cpp
+++ AndroidCore-V1/environment/functions.cpp
@@ -5,43 +5,47 @@
 #include <lgc.h>
 */
 
-static int getgenv() // register the luau first to register the lua state here
+static int getgenv(lua_State* ls) // register the luau first to register the lua state here
 {
-	// lua_pushvalue(ls, LUA_GLOBALSINDEX);
+        lua_pushvalue(ls, LUA_GLOBALSINDEX);
 	return 1;
 }
 
-static int getreg() // register the luau first to register the lua state here
+static int getreg(lua_State* ls) // register the luau first to register the lua state here
 {
-	// lua_pushvalue(ls, LUA_REGISTRYINDEX);
+	lua_pushvalue(ls, LUA_REGISTRYINDEX);
 	return 1;
 }
 
-static int gettenv() // register the luau first to register the lua state here
+static int gettenv(lua_State* ls) // register the luau first to register the lua state here
 {
-	/*
 	luaL_checktype(ls, 1, LUA_TTHREAD);
 
     auto thread = lua_tothread(ls, 1);
 
     lua_pushvalue(thread, LUA_GLOBALSINDEX);
     lua_xmove(thread, ls, 1);
-	*/
 	return 1;
 }
 
-static int getthread() // register the luau first to register the lua state here
+static int getthread(lua_State* ls) // register the luau first to register the lua state here
 {
-	// lua_pushthread(ls);
+	lua_pushthread(ls);
 	return 1;
 }
 
-static int identifyexecutor()
+static int identifyexecutor(lua_State* ls)
 {
    // will return AndroidCore 1.0.0
-   // lua_pushstring(ls, exploit_configuration::exploit_name.c_str( ));
-   // lua_pushstring(ls, exploit_configuration::exploit_version( ));
+    lua_pushstring(ls, exploit_configuration::exploit_name.c_str( ));
+    lua_pushstring(ls, exploit_configuration::exploit_version( ));
     return 2; 
+}
+
+static int isLuau(lua_State* ls)
+{
+    lua_pushboolean(ls, true); 
+    return 1;
 }
 
 static const luaL_Reg function[] = { // Should be getting an error because of unregistered luau.
@@ -50,4 +54,5 @@ static const luaL_Reg function[] = { // Should be getting an error because of un
 	{"gettenv", gettenv},
 	{"getthread", getthread},
         {"identifyexecutor", identifyexecutor},
+        {"isLuau", isLuau},
 }
